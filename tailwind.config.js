8f3d0a82b249e179858ea9f4b2940d959a0e7865
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2025-04-26 07:17:37 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          bg: '#000000',
+          text: '#ffffff',
+        },
+        secondary: {
+          text: '#9ca3af',
+        },
+        button: {
+          bg: '#e5e7eb',
+          text: '#1f2937',
+          hover: '#d1d5db',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
